fcd9dd5931b893ad89ee5ca125fec19199d22ba9
Paulo Renan <paulo.melo@squintopera.com>
2025-04-15 16:56:22 +0100

    * v0.1.3

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Squint Opera-Audio Guide-0.1.2";
+const cacheName = "Squint Opera-Audio Guide-0.1.3";
 const contentToCache = [
     "Build/Build_gzip.loader.js",
     "Build/Build_gzip.framework.js.gz",
