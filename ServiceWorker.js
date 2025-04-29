685f04fc3ead05e95aac1895c63f7c9dc6c6562f
Paulo Renan <paulo.melo@squintopera.com>
2025-04-29 17:41:31 +0100

    * Fixed background in desktop mode

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Squint Opera-Audio Guide-1.0.0";
+const cacheName = "Squint Opera-Audio Guide-1.0.1";
 const contentToCache = [
     "Build/Build_gzip.loader.js",
     "Build/Build_gzip.framework.js.gz",
