601aa387b01e9620b766a47c301f6e268699474a
Paulo Renan <paulo.melo@squintopera.com>
2025-04-30 11:59:50 +0100

    * adjusted desktop top buttons to match latest design

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Squint Opera-Audio Guide-1.0.1";
+const cacheName = "Squint Opera-Audio Guide-1.0.2";
 const contentToCache = [
     "Build/Build_gzip.loader.js",
     "Build/Build_gzip.framework.js.unityweb",
